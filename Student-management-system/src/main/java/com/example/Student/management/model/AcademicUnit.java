8a0a492ba9af6fbfb62e5b84dc82e89b9d75a879
Jessekay <kayjes00@gmail.com>
2025-10-17 11:57:21 +0200

    added the getters and setters methods to the academic unit class

--- Student-management-system/src/main/java/com/example/Student/management/model/AcademicUnit.java
+++ Student-management-system/src/main/java/com/example/Student/management/model/AcademicUnit.java
@@ -8,4 +8,45 @@ public class AcademicUnit {
     private String name;
     private EAcademicUnitType type;
     private AcademicUnit parent;
+    public UUID getId() {
+        return Id;
+    }
+    public void setId(UUID id) {
+        Id = id;
+    }
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public EAcademicUnitType getType() {
+        return type;
+    }
+    public void setType(EAcademicUnitType type) {
+        this.type = type;
+    }
+    public AcademicUnit getParent() {
+        return parent;
+    }
+    public void setParent(AcademicUnit parent) {
+        this.parent = parent;
+    }
+    public AcademicUnit() {
+    }
+    public AcademicUnit(UUID id, String code, String name, EAcademicUnitType type, AcademicUnit parent) {
+        Id = id;
+        this.code = code;
+        this.name = name;
+        this.type = type;
+        this.parent = parent;
+    }
+
+    
 }
